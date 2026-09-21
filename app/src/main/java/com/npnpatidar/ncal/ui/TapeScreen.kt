b082c245906d77c0ef72551d5c1d2bfe3a5b2e86
npnpatidar <5734298+npnpatidar@users.noreply.github.com>
2026-09-21 14:31:20 +0000

    Fix: missing rememberDrawerState import

--- app/src/main/java/com/npnpatidar/ncal/ui/TapeScreen.kt
+++ app/src/main/java/com/npnpatidar/ncal/ui/TapeScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
