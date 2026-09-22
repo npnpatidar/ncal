efdea42464c36dca6ed27e0317fcbf010eec0aec
npnpatidar <5734298+npnpatidar@users.noreply.github.com>
2026-09-22 04:06:55 +0000

    Fix: restore Color import

--- app/src/main/java/com/npnpatidar/ncal/ui/TapeScreen.kt
+++ app/src/main/java/com/npnpatidar/ncal/ui/TapeScreen.kt
@@ -61,6 +61,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
