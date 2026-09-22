871e2b534cfe4ba481b2322cff5dd6485826cf1e
npnpatidar <5734298+npnpatidar@users.noreply.github.com>
2026-09-22 14:17:45 +0000

    Fix import packages: text.selection, foundation.layout.IntrinsicSize

--- app/src/main/java/com/npnpatidar/ncal/ui/TapeScreen.kt
+++ app/src/main/java/com/npnpatidar/ncal/ui/TapeScreen.kt
@@ -13,8 +13,8 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.TextSelectionColors
-import androidx.compose.foundation.text.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.awaitEachGesture
@@ -81,9 +81,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.IntrinsicSize
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
